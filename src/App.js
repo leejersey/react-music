65c347493be7484f4b3018b43a12dd20cbce0005
jersey <lizexi711@gmail.com>
2022-04-19 11:46:55 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { IconStyle } from './assets/iconfont/iconfont';
-import routes from './routes/index.js';
-import { HashRouter, useRoutes } from 'react-router-dom';
-import { GlobalStyle } from './style';
+import React from "react";
+import { IconStyle } from "./assets/iconfont/iconfont";
+import routes from "./routes/index.js";
+import { HashRouter, useRoutes } from "react-router-dom";
+import { GlobalStyle } from "./style";
 
 const Page = () => {
   return useRoutes(routes);
-}
+};
 
 function App() {
   return (
--- src/application/Home/index.js
+++ src/application/Home/index.js
@@ -2,9 +2,7 @@
 import React from "react";
 
 function Home(props) {
-  return <div>Home111
-    
-  </div>;
+  return <div>Home111</div>;
 }
 
 export default React.memo(Home);
